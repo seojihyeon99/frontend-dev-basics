32d45c2f957845accbb11b60c79f7eb8e4209ae3
seojihyeon99 <seojihyeon99@naver.com>
2025-02-04 09:12:51 +0900

    Initial Commit

---
+++ javascript-practices/basics/ex00.js
@@ -0,0 +1,4 @@
+console.log("============ hello world1");
+console.error("============ hello world2");
+console.warn("============ hello world3");
+console.info("============ hello world4");
---
+++ javascript-practices/basics/ex02.js
@@ -0,0 +1,27 @@
+console.log("============[02.02] 표현식(Expression)");
+i = 4;
+2 + 2;
+parseInt('4');
+Math.pow(2, 2);
+4
+
+
+console.log("============[05] 세미콜론(Semicolon)");
+a = 10; b = 20;
+
+
+console.log("============[06] 개행(Line Break)");
+k
+=
+10
+console.log(k)
+
+// error: undefined error
+// console.lo
+// g('hello world')
+
+function f(x) {
+    return x * 10;
+}
+
+console.log(f(10))
